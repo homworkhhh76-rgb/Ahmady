326421b538baff1f94b2338367c97da9a36e1a8a
homworkhhh76-rgb <homworkhhh76@gmail.com>
2026-09-18 16:10:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ahmadi-users-v32.0.0';
-const ASSETS=['./','./index.html','./styles.css?v=32.0.0','./app.js?v=32.0.0','./r2-storage.js?v=30.0.0','./turso-config.js','./turso-sync.js?v=32.0.0','./manifest.webmanifest','./logo.jpg','./icon-192.png','./icon-512.png'];
+const CACHE='ahmadi-users-v33.0.0';
+const ASSETS=['./','./index.html','./styles.css?v=33.0.0','./app.js?v=33.0.0','./r2-storage.js?v=33.0.0','./turso-config.js','./turso-sync.js?v=33.0.0','./manifest.webmanifest','./logo.jpg','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
